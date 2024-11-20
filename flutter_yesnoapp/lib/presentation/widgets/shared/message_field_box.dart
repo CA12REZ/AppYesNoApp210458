5128795a6196f8c6087505e750b749b6741aac13
CA12REZ <210458@utxicotepec.edu.mx>
2024-11-20 08:09:31 -0600

    practica 23 respuesta del chat

--- flutter_yesnoapp/lib/presentation/widgets/shared/message_field_box.dart
+++ flutter_yesnoapp/lib/presentation/widgets/shared/message_field_box.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  const MessageFieldBox({super.key}); //
+
 
   @override
   Widget build(BuildContext context) {
