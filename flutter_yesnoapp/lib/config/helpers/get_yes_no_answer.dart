02e30d19dbfab48bda9aaff45c1f05cf9b025914
CA12REZ <210458@utxicotepec.edu.mx>
2024-11-21 08:08:34 -0600

    finalizando practica 23

--- flutter_yesnoapp/lib/config/helpers/get_yes_no_answer.dart
+++ flutter_yesnoapp/lib/config/helpers/get_yes_no_answer.dart
@@ -1,7 +1,15 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_yesnoapp/domain/entities/message.dart';
 
 class GetYesNoAnswer {
-  Future<Message> getAnswer() {
-    return;
+  final _dio = Dio();
+  Future<Message> getAnswer() async {
+    final response = await _dio.get('https://yesno.wtf/api');
+
+    return Message(
+    text: response.data['answer'], 
+    fromwho: Fromwho.hers,
+    imageUrl: response.data['image'], 
+    );
   }
 }
--- flutter_yesnoapp/lib/presentation/providers/chat_provider.dart
+++ flutter_yesnoapp/lib/presentation/providers/chat_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_yesnoapp/config/helpers/get_yes_no_answer.dart';
 import 'package:flutter_yesnoapp/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
 
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
   List<Message> messageList = [
     Message(text: 'como estas?', fromwho: Fromwho.me),
     Message(text: 'te puedo ver hoy?', fromwho: Fromwho.me),
@@ -12,6 +14,16 @@ class ChatProvider extends ChangeNotifier {
     if (text.isEmpty) return;
     final newMessage = Message(text: text, fromwho: Fromwho.me);
     messageList.add(newMessage);
+    if (text.endsWith('.')) {
+      herReply();
+    }
+    notifyListeners();
+    moveScrollToBotton();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(herMessage);
     notifyListeners();
     moveScrollToBotton();
   }
